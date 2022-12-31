a156c8e443aa5fe0d34b5f08ced0ad25a96578f0
Muhamad Nauval Azhar <nauvalazhar2@gmail.com>
2023-01-01 01:25:35 +0700

    add: vercel analytics

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import 'tailwindcss/tailwind.css';
 import Header from '../components/Header';
 import { Fredoka_One, Patrick_Hand } from '@next/font/google';
 import clsx from 'clsx';
+import { Analytics } from '@vercel/analytics/react';
 
 const fontDisplay = Fredoka_One({
   variable: '--font-display',
@@ -38,6 +39,7 @@ export default function RootLayout({ children }) {
             </a>
           </footer>
         </main>
+        <Analytics />
       </body>
     </html>
   );
