25b8750ce8cada4765e4d57f79bf3fd971c1ca68
Wisnu Wicaksono <63142229+wiscaksono@users.noreply.github.com>
2023-01-01 19:23:17 +0700

    add padding for responsive

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,40 +1,42 @@
-import 'tailwindcss/tailwind.css';
-import Header from 'components/Header';
-import { Fredoka_One, Patrick_Hand } from '@next/font/google';
-import clsx from 'clsx';
-import { AnalyticsWrapper } from 'components/Analytics';
+import "tailwindcss/tailwind.css";
+import Header from "components/Header";
+import { Fredoka_One, Patrick_Hand } from "@next/font/google";
+import clsx from "clsx";
+import { AnalyticsWrapper } from "components/Analytics";
 
 const fontDisplay = Fredoka_One({
-  variable: '--font-display',
-  weight: '400',
-  subsets: ['latin'],
+  variable: "--font-display",
+  weight: "400",
+  subsets: ["latin"],
 });
 
 const fontSans = Patrick_Hand({
-  variable: '--font-sans',
-  weight: '400',
-  subsets: ['latin'],
+  variable: "--font-sans",
+  weight: "400",
+  subsets: ["latin"],
 });
 
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={clsx(fontDisplay.variable, fontSans.variable, 'bg-[#1F4290]')}>
+      className={clsx(fontDisplay.variable, fontSans.variable, "bg-[#1F4290]")}
+    >
       {/*
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
       <body>
-        <main className="w-[1140px] mx-auto py-20">
+        <main className="w-[1140px] mx-auto py-20 px-10 lg:px-0">
           <Header />
           <main className="py-20">{children}</main>
           <footer className="text-brand text-center text-xl">
-            Copyright &copy; 2023{' '}
+            <p>Copyright &copy; 2023 </p>
             <a
               href="https://twitter.com/mhdnauvalazhar"
-              className="border-b-2 border-brand">
+              className="border-b-2 border-brand"
+            >
               Nauval
             </a>
             <p>Powered by Vercel</p>
