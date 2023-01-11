1eecc4366598ca56d4272f88ac3fa4596206feb0
fiqryq <fiqrychoerudin48@gmail.com>
2023-01-11 10:44:01 +0700

    chore: add .lintstagedrc config

---
+++ .lintstagedrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  // Lint & Prettify JSX and JS files
+  "**/*.(jsx|js)": (filenames) => [
+    `yarn lint`,
+    `yarn prettier --write ${filenames.join(" ")}`,
+  ],
+
+  // Prettify only JSON files
+  "**/*.(json)": (filenames) => `yarn prettier --write ${filenames.join(" ")}`,
+};
