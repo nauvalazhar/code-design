583ede8a32a1989741dfa54e7c6b56d79a505812
achmadrivaldi <mrtampan54@gmail.com>
2023-01-13 19:50:32 +0700

    edit footer

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,26 +1,26 @@
-import "tailwindcss/tailwind.css";
-import Header from "components/Header";
-import { Fredoka_One, Patrick_Hand } from "@next/font/google";
-import clsx from "clsx";
-import { AnalyticsWrapper } from "components/Analytics";
+import 'tailwindcss/tailwind.css';
+import Header from 'components/Header';
+import { Fredoka_One, Patrick_Hand } from '@next/font/google';
+import clsx from 'clsx';
+import { AnalyticsWrapper } from 'components/Analytics';
 
 const fontDisplay = Fredoka_One({
-  variable: "--font-display",
-  weight: "400",
-  subsets: ["latin"],
+  variable: '--font-display',
+  weight: '400',
+  subsets: ['latin'],
 });
 
 const fontSans = Patrick_Hand({
-  variable: "--font-sans",
-  weight: "400",
-  subsets: ["latin"],
+  variable: '--font-sans',
+  weight: '400',
+  subsets: ['latin'],
 });
 
 export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      className={clsx(fontDisplay.variable, fontSans.variable, "bg-[#1F4290]")}
+      className={clsx(fontDisplay.variable, fontSans.variable, 'bg-[#1F4290]')}
     >
       {/*
         <head /> will contain the components returned by the nearest parent
@@ -32,10 +32,12 @@ export default function RootLayout({ children }) {
           <Header />
           <main className="py-20">{children}</main>
           <footer className="text-brand text-center text-xl">
-            <p>Copyright &copy; 2023 </p>
+            <p>Copyright &copy; {new Date().getFullYear()} </p>
             <a
               href="https://twitter.com/mhdnauvalazhar"
+              target="_blank"
               className="border-b-2 border-brand"
+              rel="noreferrer"
             >
               Nauval
             </a>
