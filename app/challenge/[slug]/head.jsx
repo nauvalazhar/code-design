cd2bbb165b2eb7bea37c3a5c8d47bc0d8cb463b4
wiscaksono <wwicaksono96@gmail.com>
2023-02-02 02:46:53 +0700

    Add a titleize function

--- app/challenge/[slug]/head.jsx
+++ app/challenge/[slug]/head.jsx
@@ -1,14 +1,15 @@
 export default function Head({ params }) {
+  const titleize = (slug) =>
+    slug
+      .split("-")
+      .map(
+        (word) => word.charAt(0).toUpperCase() + word.substring(1).toLowerCase()
+      )
+      .join(" ");
+
   return (
     <>
-      <title>
-        {` The Code Design |
-        ${
-          params.slug.toString().charAt(0).toUpperCase() +
-          params.slug.toString().toLowerCase().slice(1)
-        }
-        `}
-      </title>
+      <title>{` The Code Design | ${titleize(params.slug)}`}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <meta
         name="description"
