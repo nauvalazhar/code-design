d8ed8241a7921179a8dbbe70d3b0653835fc6213
sglkc <31957516+sglkc@users.noreply.github.com>
2023-03-12 21:02:36 +0700

    feat(seo): add OpenGraph and Twitter meta tags

--- app/head.tsx
+++ app/head.tsx
@@ -2,15 +2,37 @@ export default function Head() {
   return (
     <>
       <title>Code The Design</title>
+      <meta charSet="utf-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="canonical" href="https://codedesign.dev/" />
+      <link rel="icon" href="/code-the-design.svg" />
+      <meta name="theme-color" content="#ffd466" />
+      <meta http-equiv="content-language" content="en" />
       <meta
         name="description"
         content="Level up your coding skills with hands-on design challenges."
       />
-      <link rel="icon" href="/code-the-design.svg" />
       <meta
         name="keywords"
         content="code the design, design with figma, design web, design challange"
+      />
+
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="en_US" />
+      <meta property="og:url" content="https://codedesign.dev/" />
+      <meta property="og:site_name" content="codedesign.dev" />
+      <meta property="og:title" content="Code The Design" />
+      <meta
+        property="og:description"
+        content="Level up your coding skills with hands-on design challenges."
+      />
+
+      <meta property="twitter:card" content="summary" />
+      <meta property="twitter:url" content="https://codedesign.dev/" />
+      <meta property="twitter:title" content="Code The Design" />
+      <meta
+        property="twitter:description"
+        content="Level up your coding skills with hands-on design challenges."
       />
     </>
   );
